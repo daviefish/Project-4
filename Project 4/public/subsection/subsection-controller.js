82c7beec326a66efe98845b224a538dc7e18839c
David Fisher <davie.fisher@gmail.com>
2016-05-23 11:26:45 -0700

    added checkMatch function

--- Project 4/public/subsection/subsection-controller.js
+++ Project 4/public/subsection/subsection-controller.js
@@ -15,13 +15,15 @@
 
     self.randomTweet = randomTweet;
 
+    self.checkTweeter = checkTweeter;
+
     // function that cycles through 4 seperate twitter feeds to pull a tweet
     function randomTweet() {
       var twitterProfiles = ['realDonaldTrump', 'kanyewest', 'justinbieber'];
       function getRandomInt(min, max) {
         return Math.floor(Math.random() * (max - min)) + min;
       }
-      var pick = twitterProfiles[getRandomInt(0,2)];
+      var pick = twitterProfiles[getRandomInt(0,3)];
 
 
       $http({
@@ -39,10 +41,21 @@
         var pickTweet = self.result.tweets[getRandomInt(0,99)];
 
         self.tweet = pickTweet.text;
+
+        self.tweeter = pickTweet.user.screen_name;
         console.log(self.tweet);
       });
     }
-    // randomTweet();
+
+    function checkTweeter(choice) {
+      if (choice===self.tweeter) {
+        console.log("There's a match!");
+      } else {
+        console.log("Wrong!");
+      }
+    }
+
+
 
   }
 
