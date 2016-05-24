8bab5bb76d945f3b054e149d8f743173663c1e61
David Fisher <davie.fisher@gmail.com>
2016-05-24 16:59:02 -0700

    included code for heroku

--- Project 4/server.js
+++ Project 4/server.js
@@ -7,14 +7,19 @@ var logger       = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser   = require('body-parser');
 var session      = require('express-session');
-var yaml         = require('node-yaml-config');
 var passport     = require('passport');
 var Strategy     = require('passport-local').Strategy;
 
-var app = express();
-var config = yaml.load(path.join(__dirname, 'config/app.yaml'));
+var port = process.env.PORT || 3000;
+var mongoUrl = process.env.MONGODB_URI || 'mongodb://localhost:27017/db';
+var secret = process.env || 'unicorns';
 
-require('./extras/mongoose')(config.mongodb_uri);
+var app = express();
+
+// require('./extras/mongoose')(config.mongodb_uri);
+require('./extras/mongoose')(mongoUrl);
+
+
 require('./extras/passport')(passport, Strategy);
 
 app.set('views', path.join(__dirname, 'views'));
@@ -24,7 +29,10 @@ app.use(logger('dev'));
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(session({secret: config.sessions_secret, resave: false, saveUninitialized: false}));
+
+// app.use(session({secret: config.sessions_secret, resave: false, saveUninitialized: false}));
+app.use(session({secret: secret, resave: false, saveUninitialized: false}));
+
 app.use(express.static(path.join(__dirname, 'dist')));
 
 app.use(passport.initialize());
