5d6e4535e67beb3e799e58ef6a41cf41b6e9b963
David Fisher <davie.fisher@gmail.com>
2016-05-25 16:33:37 -0700

    stuff

--- dist/js/main.js
+++ dist/js/main.js
@@ -84,6 +84,8 @@
         return Math.floor(Math.random() * (max - min)) + min;
       }
 
+
+      var GAProfiles = ['ianspringer', 'toxicityy', 'enavy04', 'SeanPypers' , 'frazood', 'FatChickenGame']
       var twitterProfiles = ['realDonaldTrump', 'kanyewest', 'justinbieber', 'fisherdavie', 'khamenei_ir', 'fatjew', 'jk_rowling', 'BarackObama', 'GA', 'maryjblige'];
 
       var pick = twitterProfiles[getRandomInt(0,10)];
@@ -164,7 +166,6 @@
   }
 
 })();
-
 
 
 
